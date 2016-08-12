557ea68514c8b1ba3e0ed8a39399c724cfe8f578
DESKTOP-ILTN8R7\thodinh <d.tr.tho@gmail.com>
2016-08-12 15:15:00 +0700

    build 2016-12-08

--- app/scripts/angularfire/firebase.ref.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.ref', ['firebase'])
-  .factory('Ref', function() {
-    'use strict';
-    return firebase.database().ref();
-  });
--- prototype-version/src/js/constants/AppConstants.js
+++
@@ -1,5 +0,0 @@
-var keyMirror = require('keymirror');
-
-module.exports = keyMirror({
-  STOCK_GETALL: null,
-});
