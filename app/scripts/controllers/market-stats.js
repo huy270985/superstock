6617aa6a6eac0cf83d5f8a44b2f5ca1a22efa710
Chau Hoang <chau.hoang@vndirect.com.vn>
2018-06-30 11:26:39 +0700

    Replace market stats with global stats completely

--- app/scripts/controllers/market-stats.js
+++ app/scripts/controllers/market-stats.js
@@ -15,52 +15,19 @@ angular.module('superstockApp')
         // so tab can highlight correctly
         $rootScope.link = link;
 
-        /*
-            1. Đếm số mã hiện lên bảng tổng hợp
-            2. Đếm số mã báo mua trên bảng tổng hợp
-            3. Số mã giao dịch >= 1 tỷ và tăng giá ( ko giảm)
-            4. Số mã giao dịch >= 1 tỷ và giảm giá
-            5. Số mã giao dịch >= 1 tỷ và uptrend
-            6. Số mã giao dịch >= 1 tỷ và downtrend
-            7. Số mã vốn hóa> 10.000 tỷ tăng giá
-            8. Số mã vốn hóa >10.000 tỷ giảm giá
-            9. Số mã giao dịch >=1 tỷ và lãi sau t+3
-            10. Số mã 1 tỷ và power >=8
-        */
-
-
-        /**
-         * @deprecated by Hungcanslim request
-        var $stats = $firebaseObject(ref.child('market_stats'));
+        var $stats = $firebaseObject(ref.child('global_stats'));
         $stats.$loaded()
             .then(function(){
+                var data = Object.keys($stats.data).map(function(key) {
+                    return {
+                        name: key,
+                        value: $stats.data[key].value,
+                        change: $stats.data[key].change,
+                    }
+                });
                 $rootScope.stats = {
-                    data: [
-                        {
-                            title: 'Báo mua/lên bảng',
-                            value: $stats.disrupt_count + '/' + $stats.summary_count,
-                        },
-                        {
-                            title: 'Giao dịch >= 1 tỷ (tăng giá/giảm giá)',
-                            value: $stats.up_with_big_value + '/' + $stats.down_with_big_value,
-                        },
-                        {
-                            title: 'Giao dịch >= 1 tỷ (uptrend/downtrend)',
-                            value: $stats.uptrend_with_big_value + '/' + $stats.downtrend_with_big_value,
-                        },
-                        {
-                            title: 'Vốn hoá >= 10,000 tỷ (tăng giá/giảm giá)',
-                            value: $stats.up_with_big_capital + '/' + $stats.down_with_big_capital,
-                        },
-                        {
-                            title: 'Giao dịch >= 1 tỷ, power >= 8',
-                            value: $stats.big_power_and_value,
-                        },
-                        {
-                            title: 'Giao dịch >= 1 tỷ, lãi sau T+3',
-                            value: $stats.big_value_and_c4_profit,
-                        },
-                    ],
+                    data: data,
+                    lastUpdated: $stats.lastUpdated,
                 };
             })
             .catch(function(error) {
@@ -72,5 +39,5 @@ angular.module('superstockApp')
                 }
                 $('#sysModal').modal('show')
             });
-        */
+
     }]);
