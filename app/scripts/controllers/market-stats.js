364d9279af88f90169f89c04d1b743421fd38b33
Chau Hoang <chau.hoang@vndirect.com.vn>
2019-01-13 17:09:07 +0700

    Add market stats

--- app/scripts/controllers/market-stats.js
+++ app/scripts/controllers/market-stats.js
@@ -9,9 +9,9 @@
  */
 angular.module('superstockApp')
     .controller('MarketStatsCtrl', ['$rootScope', '$scope', 'auth', '$firebaseArray',
-        '$firebaseObject', 'Ref', 'utils', 'currentAuth', 'link',
+        '$firebaseObject', 'Ref', 'utils', 'currentAuth', 'link', '$q',
         function ($rootScope, $scope, auth, $firebaseArray,
-            $firebaseObject, ref, $utils, currentAuth, link) {
+            $firebaseObject, ref, $utils, currentAuth, link, $q) {
         // so tab can highlight correctly
         $rootScope.link = link;
 
@@ -27,30 +27,37 @@ angular.module('superstockApp')
             }
         }
 
-        var $stats = $firebaseObject(ref.child('global_stats'));
-        $stats.$loaded()
-            .then(function(){
-                var data = Object.keys($stats.data).map(function(key) {
-                    return {
-                        name: key,
-                        value: $stats.data[key].value,
-                        change: $stats.data[key].change,
-                        color: change_color($stats.data[key].change_value)
-                    }
-                });
-                $rootScope.stats = {
-                    data: data,
-                    lastUpdated: $stats.lastUpdated,
-                };
-            })
-            .catch(function(error) {
-                var message = 'Không lấy được thống kê thị trường, xin thử lại.'
-                console.error(message, error);
-                $rootScope.system = {
-                    message: message,
-                    error: error
+        function toArray(obj) {
+            return Object.keys(obj).map(function(key) {
+                return {
+                    name: obj[key].id,
+                    value: obj[key].value,
+                    change: obj[key].change,
+                    color: change_color(obj[key].change_value)
                 }
-                $('#sysModal').modal('show')
             });
+        }
+
+        var $indices = $firebaseObject(ref.child('stats/indices'));
+        var $commodities = $firebaseObject(ref.child('stats/commodities'));
+        $q.all([$indices.$loaded(), $commodities.$loaded()]).then(function() {
+            $rootScope.stats = {
+                indices: {
+                    data: toArray($indices.data),
+                },
+                commodities: {
+                    data: toArray($commodities.data),
+                },
+            }
+        })
+        .catch(function(error) {
+            var message = 'Không lấy được thống kê thị trường, xin thử lại.'
+            console.error(message, error);
+            $rootScope.system = {
+                message: message,
+                error: error
+            }
+            $('#sysModal').modal('show')
+        });
 
     }]);
